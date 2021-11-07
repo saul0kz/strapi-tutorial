d8316d9f0807eeccc136cc96599e980408927cfa
Saulo <saulo.monteiro.dev@gmail.com>
2021-11-07 10:46:27 -0300

    first commit with backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fdef0aab6c61cb614780005dd50c8c69'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1286985a-3630-4166-b779-71ceb45c069b'
+};
