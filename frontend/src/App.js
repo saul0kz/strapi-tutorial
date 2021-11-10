c7c3796038715d14793584ff72897683aa58b6dd
Saulo <saulo.monteiro.dev@gmail.com>
2021-11-10 06:56:38 -0300

    useQuery in homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import ReviewDetails from "pages/ReviewDetails";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:1337",
+  uri: "http://localhost:1337/graphql",
   cache: new InMemoryCache(),
 });
 
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,14 +1,27 @@
-import useFetch from "hooks/useFetch";
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { useQuery, gql } from '@apollo/client'
+
+const REVIEWS = gql`
+  query GetReviews {
+    reviews {
+      title,
+      body,
+      rating,
+      id
+    }
+  }
+`
+
+
 export default function Homepage() {
-  const { loading, error, data } = useFetch("http://localhost:1337/reviews");
+  const { loading, error, data } = useQuery(REVIEWS);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>error :()</p>;
   return (
     <div>
-      {data?.map((review) => (
+      {data?.reviews.map((review) => (
         <div key={review.id} className="review-card">
           <div className="ratting">{review.rating}</div>
           <h2>{review.title}</h2>
